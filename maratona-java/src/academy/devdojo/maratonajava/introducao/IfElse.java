ae5f67f1c56c763e3d0e195127cb04d39e7b938d
Vicente <abacherli07@gmail.com>
2025-10-25 00:42:29 -0300

    Projeto pra postar linkedin

--- maratona-java/src/academy/devdojo/maratonajava/introducao/IfElse.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/IfElse.java
@@ -2,18 +2,23 @@ package academy.devdojo.maratonajava.introducao;
 
 public class IfElse {
     public static void main(String[] args) {
-        String fruta = "maçã";
+        String fruta = "melancia";
 
-        if (fruta == "maçã") {
-            System.out.println("A maça custa $2,19 o kg!");
-            new IfElse (fruta == "melancia")
-            System.out.println();
-
-
-
-
-
+        if (fruta.equals("maçã")) {
+            System.out.println("A maçã custa $2,19 o kg!");
+        } else if (fruta.equals("melancia")) {
+            System.out.println("A melancia custa $1,00 o kg!");
+        } else if (fruta.equals("melão")) {
+            System.out.println("O melão custa $4,99 o kg!");
+        } else if (fruta.equals("banana")) {
+            System.out.println("A banana custa $3,59 o kg!");
+        } else if (fruta.equals("manga")) {
+            System.out.println("A manga custa $4,19 o kg!");
+        } else {
+            System.out.println("Não temos " + fruta + ".");
         }
 
+        System.out.println("Gostaria de mais alguma coisa?");
     }
 }
+
