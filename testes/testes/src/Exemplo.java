1df8bfff0e966818b8be545f00bf7f83080dfd10
Vicente <163614123+VicenteAbacherli@users.noreply.github.com>
2026-01-29 16:18:41 -0300

    Começando uns testes em Java.

---
+++ testes/testes/src/Exemplo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import meuPacote.validaCPF;
+
+public class Exemplo {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        String CPF;
+
+        System.out.print("Informe um CPF (somente números): ");
+        CPF = ler.next();
+
+        System.out.print("\nResultado: ");
+        if (validaCPF.isCPF(CPF)) {
+            System.out.println(validaCPF.imprimeCPF(CPF));
+        } else {
+            System.out.println("Erro, CPF inválido!");
+        }
+    }
+}
--- testes/testes/src/meuPacote/validaCPF.java
+++ testes/testes/src/meuPacote/validaCPF.java
@@ -1,4 +1,63 @@
 package meuPacote;
 
+import java.util.InputMismatchException;
+
 public class validaCPF {
+
+    public static boolean isCPF(String CPF) {
+
+        if (CPF.equals("00000000000") ||
+                CPF.equals("11111111111") ||
+                CPF.equals("22222222222") ||
+                CPF.equals("33333333333") ||
+                CPF.equals("44444444444") ||
+                CPF.equals("55555555555") ||
+                CPF.equals("66666666666") ||
+                CPF.equals("77777777777") ||
+                CPF.equals("88888888888") ||
+                CPF.equals("99999999999") ||
+                (CPF.length() != 11))
+            return false;
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        try {
+            // 1º dígito verificador
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+                num = CPF.charAt(i) - 48;
+                sm += num * peso;
+                peso--;
+            }
+
+            r = 11 - (sm % 11);
+            dig10 = (r == 10 || r == 11) ? '0' : (char) (r + 48);
+
+            // 2º dígito verificador
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = CPF.charAt(i) - 48;
+                sm += num * peso;
+                peso--;
+            }
+
+            r = 11 - (sm % 11);
+            dig11 = (r == 10 || r == 11) ? '0' : (char) (r + 48);
+
+            return (dig10 == CPF.charAt(9) && dig11 == CPF.charAt(10));
+
+        } catch (InputMismatchException e) {
+            return false;
+        }
+    }
+
+    public static String imprimeCPF(String CPF) {
+        return CPF.substring(0, 3) + "." +
+                CPF.substring(3, 6) + "." +
+                CPF.substring(6, 9) + "-" +
+                CPF.substring(9, 11);
+    }
 }
